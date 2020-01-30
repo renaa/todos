997d0421b0dabc4f7977dfd7acdda579e9fe5d87
tomas <tomasrenaa@gmail.com>
2020-01-30 22:34:21 +0100

    working prototype, added todos

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
   <div id="app">
-    <span>Create new task</span>
-    <test msg="Welcome to Your Vue.js App"/>
+    <span>Create new task //todo pass name </span>
+    <button @click="onClick">Click to insert</button>
+
+    <div ref="container" class="container">
+      <timer name="task1" />
+      <timer name="task2" />
+    </div>
   </div>
 </template>
 
 <script>
-import test from './components/test.vue'
+import Vue from "vue";
+import timer from "./components/timer.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    test
+    timer
+  },
+  methods: {
+    onClick() {
+      var componentClass = Vue.extend(timer);
+      var instance = new componentClass({
+        propsData: { type: "primary" }
+      });
+      instance.$slots.default = ["click me!"];
+      instance.$mount();
+      this.$refs.container.appendChild(instance.$el);
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  position: fixed;
+  padding: 50px 0 0 0;
+  margin: 0;
+
+  top: 0;
+  left: 0;
+
+  width: 100%;
+  height: 100%;
+  background: blanchedalmond;
+
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.container {
+  display: flex;
+  -ms-flex-align: center;
+  flex-direction: row;
+  justify-content: center;
 }
 </style>
---
+++ src/components/timer.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="timer" :class='{activetimer: running}'>
+    <!-- //https://github.com/vuejs/vue/issues/6677    error: v-bind without argument expects an Object or Array value: -->
+    <input class='name' :class='{activetimer: running}' type='text' v-bind='{toString: () => name}' /> 
+    <button class="timer-button" :class='{active: running}' @click="start">start</button>
+    <button class="timer-button" :class='{active: !running}' @click="stop">stop</button>
+    <div class="counter" :class='{activecounter: running}'>{{displayTime}}</div>
+
+    <button class="destroy-button" v-on:click='destroy'>X</button> <!-- //todo implement destroy -->
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "counter",
+  props: {
+    name: String
+  },
+  data() {
+    return {
+      running: false,
+      startTime: 0,
+      displayTime: '00:00:00',
+      counter: 0,
+      timerFunction: Function
+    };
+  },
+  computed: {},
+
+  methods: {
+    start: function() {
+      if (!this.running) {
+        this.running = true;
+        this.timerFunction = setInterval(this.updateTime, 1000);
+      }
+    },
+
+    updateTime: function() {
+      ++this.counter;
+      this.displayTime = this.prettyDisplay(this.counter);
+    },
+    prettyDisplay(c){
+      var sec_num = parseInt(c, 10); // don't forget the second param
+      var hours   = Math.floor(sec_num / 3600);
+      var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+      var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+      if (hours   < 10) {hours   = "0"+hours;}
+      if (minutes < 10) {minutes = "0"+minutes;}
+      if (seconds < 10) {seconds = "0"+seconds;}
+      return hours+':'+minutes+':'+seconds;
+    },
+    stop: function() {
+      if (this.running) this.running = false;
+      clearInterval(this.timerFunction);
+    },
+    destroy: function() {
+      //todo
+      this.$el.parentNode.removeChild(this.$root.$el)
+    }
+  },
+  
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped >
+
+.timer {
+  /* primary and secondary color */
+  --p:  #267552;
+  --s: #a5e7bb;
+  --p:  #740c54;
+  --s: #ecd46b;
+  --p:  #421e09;
+  --s: #ebb38d;
+  position: relative;
+  padding: 20px 25px 20px 20px;
+  margin: 10px;
+  border: 2px solid var(--p);
+  width: fit-content;
+  border-radius: 5px;
+  background-color: var(--s);
+}
+.name {
+  text-align: center;
+  font-size: 20px;
+  display: block;
+  background-color: var(--s);
+  border:2px solid var(--p);
+  color: var(--p);
+  border-radius: 5px;
+  margin:  10px;
+  padding: 5px;
+  outline: 0;
+}
+
+.counter {
+  padding: 15px 0 0;
+  font-size: 25px;
+  color: var(--p);
+}
+.activecounter {
+  font-weight: bold ;
+}
+.timer-button {
+  border: none;
+  background-color: var(--s);
+  cursor: pointer;
+  color: var(--p);
+  font-size: 16px;
+  padding: 10px;
+  margin: 10px;
+  border-radius: 3px;
+  border: 2px solid var(--p);
+}
+.active{
+  background-color: var(--p);
+  color:var(--s);
+}
+.timer-button:focus {
+  outline: 0;
+  
+}
+.destroy-button {
+  border: none;
+  background-color: #0000;
+  color: var(--p);
+  position: absolute;
+  top: -2px;
+  right: -2px;
+  border: 2px solid var(--p);
+  border-radius: 5px;
+  padding: 5px 8px;
+
+}
+.destroy-button:focus{
+  outline: 0;
+}
+</style>
