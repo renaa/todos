31edf47c657d90e07b86cc30c827d064b7e743a6
tomas <tomasrenaa@gmail.com>
2020-02-01 16:46:27 +0100

    had some fun with colors

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <template>
-  <div id="app">
-    <input id='createTimerName' class='createTimerName' type='text' v-on:keyup.enter="onClick(event)" v-model="inputTimerName" /> 
+  <div id="app" :class="{runningTimer: runningInstances > 0}">
+    <input
+      id="createTimerName"
+      class="createTimerName"
+      type="text"
+      v-on:keyup.enter="onClick()"
+      v-model="inputTimerName"
+    />
 
     <!-- <button @click="onClick">Click to insert</button> -->
-    <div v-if="timerList.length > 0"  ref="container" class="container">
-
-    <timer :timerName='time' v-for="(time,i) in timerList" :key="i">
-        <input type="text">
-    </timer>
-      
+    <div v-if="timerList.length > 0" class="container">
+      <timer
+        v-for="(time, index) in timerList"
+        :key="time+index.toString()"
+        v-on:onStart="onStart"
+        v-on:onStop="onStop"
+        v-on:destroyMe="removeTimer(index)"
+        
+        :timerName="time"
+        :p='getRandomColor(75, 75)' 
+        :s='getRandomColor(180, 75)'
+      ></timer>
     </div>
   </div>
 </template>
@@ -20,35 +32,66 @@ import timer from "./components/timer.vue";
 export default {
   name: "app",
   components: {
-      timer
+    timer
   },
   data: function() {
-      return {
-        inputTimerName: '',
-        timerList: []
-      }
+    return {
+      inputTimerName: "",
+      timerList: [],
+      runningInstances: 0
+    };
   },
   methods: {
     onClick: function() {
-    this.timerList.push(this.inputTimerName);
-    this.inputTimerName = ''
+      this.timerList.push(this.inputTimerName);
+      this.inputTimerName = "";
     },
-    doLOLS: function(){
-      console.log("CLICK FROM OUTSIDE OF COMPONENT");
-    }
-  },
-  created(){
+    removeTimer: function(index) {
+      this.timerList.splice(index, 1);
+    },
+    onStart: function() {
+      this.runningInstances++;
+    },
+    onStop: function() {
+      this.runningInstances--;
+    },
+    getRandomColor: function(value, variance) { // dont feed me numbers above 255 - variance/2
 
-    var app = document.querySelector("#app");
-    app.style.setProperty('background', "#44af44");
-    console.log("....")
-    console.log("background", "#aaa" )
+      var r = this.getrandomColorChannel(value, variance, false)
+      var g = this.getrandomColorChannel(value, variance, false)
+      var b = this.getrandomColorChannel(value, variance, false)
+      var returnValue =  '#' + this.getHex(r) + this.getHex(g) + this.getHex(b)
+      //console.log(returnValue, this.timerList.length)
+      return returnValue
+    },
+    getrandomColorChannel(value, variance, toneDown){
+      var x = Math.random()*variance*2 + value - variance;
+      if (toneDown){
+        x = x * 0.8;
+      }
+      return Math.floor(x)
+    },
+    getHex: function(value) {
+      var hex = '00'
+      if (value > 0 && value < 255){
+        hex = value.toString(16) 
+        if (hex.length < 2){
+          hex = '0' + hex
+        }
+      }
+      return hex
+    }
+    
   },
+  created() {
+    //var app = document.querySelector("#app");
+    //app.style.setProperty("background", "#44af44");
+
+  }
 };
 </script>
 
 <style>
-
 #app {
   --background-color: #af4444;
   --foreground-color: #000;
@@ -69,6 +112,9 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
+.runningTimer {
+  background-color: #000;
+}
 .container {
   display: flex;
   flex-wrap: wrap;
@@ -82,7 +128,7 @@ export default {
   font-size: 20px;
   display: block;
   background-color: transparent;
-  border:2px solid var(--foreground-color);
+  border: 2px solid var(--foreground-color);
   color: var(--foreground-color);
   border-radius: 5px;
   padding: 5px;
